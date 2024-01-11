85d690b017aa0a92b8fd438a3c54b4f735f26eba
Maciej Jankowski <jankowski.maciej0@gmail.com>
2024-01-11 10:52:21 +0100

    ADD: Add img for readme file.

--- src/main/java/com/tsl/TslApplication.java
+++ src/main/java/com/tsl/TslApplication.java
@@ -2,10 +2,8 @@ package com.tsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TslApplication {
 
 	public static void main(String[] args) {
